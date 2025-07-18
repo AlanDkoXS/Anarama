2e70089dc79b910dc53f0531f156c1bd219cb389
Alan Quintana <alan.quintanapacheco@gmail.com>
2025-07-17 20:37:17 -0600

    :computer: TIME TO CODE: Initial commit

---
+++ src/i18n/traducciones.md
@@ -0,0 +1,3 @@
+Para ver la documentación
+
+[Astro i18n](https://docs.astro.build/en/recipes/i18n/)
---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+  },
+  es: {
+    'nav.home': 'Inicio',
+    'nav.about': 'Acerca de',
+  },
+} as const;
